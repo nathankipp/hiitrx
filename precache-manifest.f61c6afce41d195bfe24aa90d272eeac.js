3dddb589cdbc3e081602e6062af972a6bccc7425
Nathan Kipp <nskipp@gmail.com>
2020-08-06 12:14:13 -0500

    Updates

---
+++ precache-manifest.f61c6afce41d195bfe24aa90d272eeac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5531714b82bdd9f045bc127816aa21e6",
+    "url": "/tri-tap/index.html"
+  },
+  {
+    "revision": "47409f3b896536ac6dd9",
+    "url": "/tri-tap/static/css/main.5f944f64.chunk.css"
+  },
+  {
+    "revision": "33b2d4620d59d075f73f",
+    "url": "/tri-tap/static/js/2.b2e189de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tri-tap/static/js/2.b2e189de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47409f3b896536ac6dd9",
+    "url": "/tri-tap/static/js/main.b9975a14.chunk.js"
+  },
+  {
+    "revision": "4b42ee4d78d9434364ea",
+    "url": "/tri-tap/static/js/runtime-main.3875c67d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tri-tap/static/media/logo.5d5d9eef.svg"
+  }
+]);
