21cc585402ed63c7f79756f9091b8c524754bc37
Nathan Kipp <nskipp@gmail.com>
2020-08-07 11:37:42 -0500

    hash router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import Lift from './Lift';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
