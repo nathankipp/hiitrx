96aadfcf36833254f46c9ad091cff6ee77385ca3
Nathan Kipp <nskipp@gmail.com>
2020-08-31 09:50:19 -0500

    add data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LS from './ls';
 import Lift from './Lift';
 import Tap from './Tap';
 import Tform from './Tform';
+import Data from './Data';
 
 export default function App() {
   const [isValid, setIsValid] = useState(false);
@@ -31,34 +32,44 @@ export default function App() {
 
   return (
     <Router>
-      <div className="wrapper">
-        <div className="links">
-          {isValid && <Link to="/tap">Tap</Link>}
-          {isValid && <Link to="/lift">Lift</Link>}
-          <Link to="/user">
-            <div className="user">
-              {
-                [
-                  new Date().toLocaleDateString(),
-                  LS.getItem('name'),
-                ].map(t => t ? <>{t}<br /></> : null)
-              }
+      <Switch>
+        <Route
+          path="/data/:table(lift|tap)"
+          render={rp => <Data rp={rp} />}
+        />
+        <Route>
+          <div className="wrapper">
+            <div className="links">
+              {isValid && <Link to="/tap">Tap</Link>}
+              {isValid && <Link to="/lift">Lift</Link>}
+              <Link to="/user">
+                <div className="user">
+                  {
+                    [
+                      new Date().toLocaleDateString(),
+                      LS.getItem('name'),
+                    ].map(t => t ? <>{t}<br /></> : null)
+                  }
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        {isValid
-          ? <Switch>
-              <Route path="/lift">
-                <Lift />
-              </Route>
-              <Route path="/tap">
-                <Tap />
-              </Route>
-              <Route render={() => <Tform onSubmit={onSubmit} />} />
-            </Switch>
-          : <Route render={() => <Tform onSubmit={onSubmit} />} />
-        }
-      </div>
+            {isValid
+              ? <Switch>
+                  <Route path="/lift">
+                    <Lift />
+                  </Route>
+                  <Route path="/tap">
+                    <Tap />
+                  </Route>
+                  <Route>
+                    <Tform onSubmit={onSubmit} />}
+                  </Route>
+                </Switch>
+              : <Tform onSubmit={onSubmit} />
+            }
+          </div>
+        </Route>
+      </Switch>
     </Router>
   );
 }
--- src/items.js
+++ src/items.js
@@ -1,20 +1,25 @@
 import { v4 as uuid } from 'uuid';
+import LS from './ls';
 
-const USER = 'unspecified';
+const DEFAULTS = {
+  name: 'unspecified',
+  age: 'unspecified',
+};
+
+const lsData = () => LS.items.map(({ field }) => ({ [field]: LS.getItem(field) || DEFAULTS[field] }));
 
 export function liftItem(data) {
   const {
     id = uuid(),
     timestamp = Date.now(),
-    user = window.sessionStorage.getItem('user') || USER,
     winNavUa = window.navigator.userAgent,
   } = data;
 
   return {
     id,
     timestamp,
-    user,
     winNavUa,
-    ...data
+    ...lsData(),
+    ...data,
   };
 }
