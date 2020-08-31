edb9c175e217a432ce814dbf5a58aafd033abee3
Nathan Kipp <nathankipp@users.noreply.github.com>
2020-08-31 09:51:06 -0500

    add data (#3)

--- src/db.js
+++ src/db.js
@@ -9,20 +9,17 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 const dynamo = new AWS.DynamoDB.DocumentClient();
 // const TableName = 'cyclog';
 
-// export function fetchRides() {
-//   return new Promise((resolve, reject) => {
-//     dynamo.scan({ TableName }, (err, data) => {
-//       if (err) {
-//         reject('ride list cannot be retrieved');
-//       } else {
-//         resolve(data.Items.map(ride => ({
-//           ...ride,
-//           color: COLORS.black,
-//         })));
-//       }
-//     });
-//   });
-// }
+export function fetchData(TableName) {
+  return new Promise((resolve, reject) => {
+    dynamo.scan({ TableName }, (err, data) => {
+      if (err) {
+        reject('data cannot be retrieved');
+      } else {
+        resolve(data.Items);
+      }
+    });
+  });
+}
 
 export function putItemInTable(Item, TableName) {
   return new Promise((resolve, reject) => {
